9a55f02b0e381300010e2d034cd4caf074fd2922
Marcos Vinicius dos Reis Alves <75844594+Marcos1110@users.noreply.github.com>
2023-03-03 11:55:23 -0300

    Add files via upload

---
+++ SeqDeFibonacci/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeqDeFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de Variáveis
+
+            //Início
+
+            int numEscolhido       = 0;
+            int primeiroValor      = 0;
+            int segundoValor       = 1;
+            int valorAuxiliar      = 0;
+            Boolean statusPesquisa = false;
+            
+            //Fim
+
+            Console.WriteLine("Sequência de Fibonacci");
+            Console.WriteLine("");
+
+            Console.WriteLine("O objetivo desse programa é verificar se determinado número" +
+                "se faz presenta na sequência de Fibonacci.");
+            Console.Write("Insira aqui o número que deseja pesquisar = ");
+            numEscolhido = int.Parse(Console.ReadLine());
+
+            while (primeiroValor < numEscolhido)
+            {
+                valorAuxiliar = primeiroValor;
+                primeiroValor = segundoValor;
+                segundoValor += valorAuxiliar;
+                if(primeiroValor == numEscolhido)
+                {
+                    statusPesquisa = true;
+                }
+            }
+
+            if (statusPesquisa)
+            {
+                Console.WriteLine("O número [" + numEscolhido + "] está presente na sequência de Fibonacci.");
+            } else
+            {
+                Console.WriteLine("O número [" + numEscolhido + "] não está presente na sequência de Fibonacci.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ inverterString/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inverterString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txtEntrada = "";
+            int contAuxiliar  = 0;
+
+            Console.WriteLine("Insira uma cadeia de caracteres (palavra, frase, número, etc.)" +
+                " que você deseja inverter");
+            Console.Write("-> ");
+            txtEntrada = Console.ReadLine();
+
+            char [] vetorInversao = new char[txtEntrada.Length];
+            contAuxiliar = txtEntrada.Length - 1;
+
+            for (int i = 0; i < txtEntrada.Length; i++)
+            {
+                vetorInversao[i] = txtEntrada[contAuxiliar];
+                contAuxiliar--;
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Cadeia de caracteres inserida  = " + txtEntrada);
+            Console.Write("Cadeia de caracteres invertida = ");
+            for (int j = 0; j < vetorInversao.Length; j++)
+            {
+                Console.Write(vetorInversao[j]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
